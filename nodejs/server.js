ac2f35c5ca42bf2925ec82309454a679f1fe9047
Bhawna Dey <99659191+bhawna-dey@users.noreply.github.com>
2022-02-21 12:09:22 +0530

    Add files via upload

---
+++ nodejs/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const res = require('express/lib/response');
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/static/index.html');
+  });
+
+app.get('/form', (req, res) => {
+    res.sendFile(__dirname + '/static/form.html');
+});
+
+
+app.post('/form', (req, res) =>{
+    let username = req.body.username;
+    let password = req.body.password;
+    if(username == "admin" && password == "user")
+{
+    res.send(`User login sucessful: Username: ${username} Password:${password}`)
+}
+     else {
+        res.status({ status: 401 }).json({ message:'Unauthorized user' });
+}
+});
+
+const port = 8080
+
+app.listen(port, () => console.log(`This app is listening on port ${port}`));
